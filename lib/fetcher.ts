e8cdae87e03d689c665cbdbe59577d321d7283bc
tamht <tamht@kozo-japan.com>
2023-11-09 11:47:39 +0700

    fix access-control-allow-origin

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
+// import axiosClient from './axiosClient';
 
 const fetcher = (url: string) =>
   axios
-    .get(url)
+    .get(url, {
+      headers: { 'Access-Control-Allow-Origin': '*' },
+    })
     .then((res) => res.data)
     .catch((err) => console.log('fetch error', err));
 
