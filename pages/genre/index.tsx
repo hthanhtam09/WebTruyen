144c732e0ac48d1e18930314190f4981db8a25ef
tamht <tamht@kozo-japan.com>
2023-11-11 14:53:15 +0700

    add favicon and filter

--- pages/genre/index.tsx
+++ pages/genre/index.tsx
@@ -1,12 +1,60 @@
+import FilterFilm from '@/components/FilterFilm';
 import LayoutHeader from '@/components/LayoutHeader';
-import React from 'react';
+import useMovieList from '@/hooks/useMovieList';
+import React, { useState } from 'react';
+import Loading from '../loading';
+import { useRouter } from 'next/router';
+import PlayButton from '@/components/PlayButton';
+import MovieList from '@/components/MovieList';
+import InfoModal from '@/components/InfoModal';
+import useInfoModalStore from '@/hooks/useInfoModalStore';
 
-const genreScreen = () => {
+const GenreScreen = () => {
+  const [sortValue, setSortValue] = useState('newest');
+  const [genreValue, setGenreValue] = useState('action');
+  const [countryValue, setCountryValue] = useState('us');
+
+  const { data: movies = [], isLoading } = useMovieList();
+  const router = useRouter();
+  const { isOpen, closeModal } = useInfoModalStore();
+
+  const handleSortChange = (value: string) => {
+    setSortValue(value);
+    // Xử lý khi giá trị sắp xếp thay đổi
+  };
+
+  const handleGenreChange = (value: string) => {
+    setGenreValue(value);
+    // Xử lý khi giá trị thể loại thay đổi
+  };
+
+  const handleCountryChange = (value: string) => {
+    setCountryValue(value);
+    // Xử lý khi giá trị quốc gia thay đổi
+  };
   return (
     <div className="pt-[93px]">
       <LayoutHeader />
+      <div className="pb-40">
+        <FilterFilm
+          onSortChange={handleSortChange}
+          onGenreChange={handleGenreChange}
+          onCountryChange={handleCountryChange}
+        />
+
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <>
+            <div className="flex-col px-4 md:px-16 py-6 flex items-start transition duration-500 bg-zinc-900 bg-opacity-90">
+              <MovieList title="" data={movies} style="mt-10" />
+            </div>
+            <InfoModal visible={isOpen} onClose={closeModal} />
+          </>
+        )}
+      </div>
     </div>
   );
 };
 
-export default genreScreen;
+export default GenreScreen;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Metadata, NextPageContext } from 'next';
+import { NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
 
 import Billboard from '@/components/Billboard';
 import MovieAlbum from '@/components/MovieAlbum';
 import useTrans from '@/hooks/useTrans';
+import Head from 'next/head';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -23,16 +24,15 @@ export async function getServerSideProps(context: NextPageContext) {
   };
 }
 
-export const metadata: Metadata = {
-  title: 'CineShin',
-  description: 'CineShin and Chill',
-};
-
 const Home = () => {
   const trans = useTrans();
 
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <title>CineShin</title>
+      </Head>
       <Billboard />
       <div className="pb-40">
         <MovieAlbum title={trans.home.trending} />
