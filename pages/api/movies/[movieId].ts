f25cb61795a49bd4270be76020d2eba3cddc9c52
tamht <tamht@kozo-japan.com>
2023-12-08 11:52:43 +0700

    update source

--- pages/api/movies/[movieId].ts
+++ pages/api/movies/[movieId].ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import prismadb from '@/lib/prismadb';
-import serverAuth from "@/lib/serverAuth";
+// import serverAuth from "@/lib/serverAuth";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
@@ -8,7 +8,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       return res.status(405).end();
     }
 
-    await serverAuth(req, res);
+    // await serverAuth(req, res);
 
     const { movieId } = req.query;
 
