97e5364469f9d2923162e31e3ac04be2d728f43f
tamht <tamht@kozo-japan.com>
2023-12-15 11:52:58 +0700

    update source

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,18 @@ import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import Head from 'next/head';
 
 function App({ Component, pageProps: { ...pageProps } }: AppProps) {
   return (
     <>
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4979943891567316"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
