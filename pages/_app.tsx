0aa5a8e590a602eecaf0fc136b9ff04570451501
tamht <tamht@kozo-japan.com>
2023-12-24 00:00:47 +0700

    fix warn nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,27 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import ClientOnly from './ClientOnly';
 import Script from 'next/script';
+import { useEffect } from 'react';
 
-function App({ Component, pageProps: { ...pageProps } }: AppProps) {  
+function App({ Component, pageProps: { ...pageProps } }: AppProps) {
+
   return (
     <ClientOnly>
-       <Script
+      <Script
+        id="Script1"
         async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4979943891567316"
-        strategy="lazyOnload"
-        crossOrigin="anonymous"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      />
+      <Script
+        id="Script2"
+        dangerouslySetInnerHTML={{
+          __html: `
+               (adsbygoogle = window.adsbygoogle || []).push({
+                   google_ad_client: "ca-pub-4979943891567316",
+                   enable_page_level_ads: true
+              });
+                `,
+        }}
       />
       <Navbar />
       <Component {...pageProps} />
