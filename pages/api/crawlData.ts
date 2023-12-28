ce450943d20290c1c0eeb63febef8d372afb40cb
tamht <tamht@kozo-japan.com>
2023-12-28 08:21:01 +0700

    update crawl

--- pages/api/crawlData.ts
+++ pages/api/crawlData.ts
@@ -6,28 +6,77 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import connectDB from '@/lib/db';
 
 let currentPage = 1;
-const maxPages = 1000;
+const maxPages = 1044;
+
+async function checkMovieData(slug: string): Promise<boolean> {
+  const movieEndpoint = `https://ophim1.com/phim/${slug}`;
+
+  try {
+    const response = await axios.get(movieEndpoint);
+
+    if (response.status === 200) {
+      const movieData = response.data;
+
+      if (
+        movieData.movie.name !== '' &&
+        movieData.movie.origin_name !== '' &&
+        movieData.movie.content !== '' &&
+        movieData.movie.type !== '' &&
+        movieData.movie.thumb_url !== '' &&
+        movieData.movie.trailer_url !== '' &&
+        movieData.movie.time !== '' &&
+        movieData.movie.episode_current !== '' &&
+        movieData.movie.episode_total !== '' &&
+        movieData.movie.quality !== '' &&
+        movieData.movie.lang !== '' &&
+        movieData.movie.slug !== '' &&
+        movieData.movie.year !== undefined &&
+        movieData.movie.poster_url !== '' &&
+        movieData.episodes[0].server_name !== '' &&
+        movieData.episodes[0].server_data.length > 0
+      ) {
+        return true;
+      }
+      return false;
+    } else {
+      return false;
+    }
+  } catch (error: any) {
+    console.error(`Error fetching data for ${slug}: ${error.message}`);
+    return false;
+  }
+}
 
 async function crawlAndStoreData(page: number) {
   const endpoint = `https://ophim1.com/danh-sach/phim-moi-cap-nhat?page=${page}`;
 
   try {
     const response = await axios.get(endpoint);
-    console.log('response', response.data.items);
+
     if (response.status === 200) {
       const movies = response.data.items;
       for (const movie of movies) {
-        try {
-          const existingMovie = await Movie.findOne({ _id: movie._id });
+        const slug = movie.slug;
 
-          if (!existingMovie) {
-            const newMovie = new Movie(movie);
-            await newMovie.save();
-          } else {
-            console.log(`Bỏ qua: ${movie._id} - Movie already exists in the database`);
+        // Check movie data before processing
+        const isValidMovie = await checkMovieData(slug);
+
+        if (isValidMovie) {
+          try {
+            const existingMovie = await Movie.findOne({ _id: movie._id });
+
+            if (!existingMovie) {
+              const newMovie = new Movie(movie);
+              console.log('Movie Valid: ', newMovie._id);
+              await newMovie.save();
+            } else {
+              console.log(`Bỏ qua: ${movie._id} - Movie already exists in the database`);
+            }
+          } catch (error: any) {
+            console.error(`Error: ${error.message}`);
           }
-        } catch (error: any) {
-          console.error(`Error: ${error.message}`);
+        } else {
+          console.log('Movie invalid');
         }
       }
     } else {
@@ -39,9 +88,8 @@ async function crawlAndStoreData(page: number) {
 }
 
 const cronJob = cron.schedule(
-  '*/10 * * * * *',
+  '*/2 * * * * *',
   () => {
-    console.log('cron job');
     if (currentPage > maxPages) {
       console.log('Đã đạt tới số trang tối đa. Dừng cron job.');
       cronJob.stop();
