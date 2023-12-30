a1adfa87b0742cff91a5dabdf6bfd7a2eb79ff35
tamht <tamht@kozo-japan.com>
2023-12-30 23:24:07 +0700

    change position sw file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,23 +4,28 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import ClientOnly from './ClientOnly';
 import { useEffect } from 'react';
+import Script from 'next/script';
 
 function App({ Component, pageProps: { ...pageProps } }: AppProps) {
-
   useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/sw.js')
-        .then(registration => {
-          console.log('Service Worker registered with scope:', registration.scope);
-        })
-        .catch(error => {
-          console.error('Service Worker registration failed:', error);
-        });
-    }
+    (async () => {
+      try {
+        if ('serviceWorker' in navigator) {
+          const registration1 = await navigator.serviceWorker.register('/sw1.js');
+          console.log('Service Worker 1 registered with scope:', registration1.scope);
+
+          const registration2 = await navigator.serviceWorker.register('/sw2.js');
+          console.log('Service Worker 2 registered with scope:', registration2.scope);
+        }
+      } catch (error) {
+        console.error('Service Worker registration failed:', error);
+      }
+    })();
   }, []);
 
   return (
     <ClientOnly>
+      <Script data-cfasync="false" src="//thubanoa.com/1?z=6835935" />
       <Navbar />
       <Component {...pageProps} />
       <Footer />
