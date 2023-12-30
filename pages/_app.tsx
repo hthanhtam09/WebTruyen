5ae475218c3769ced1cc8ebc6011e5c0f3c9a8c3
tamht <tamht@kozo-japan.com>
2023-12-30 17:45:53 +0700

    remove file adsense

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,28 +3,11 @@ import '../styles/globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import ClientOnly from './ClientOnly';
-import Script from 'next/script';
 
 function App({ Component, pageProps: { ...pageProps } }: AppProps) {
 
   return (
     <ClientOnly>
-      <Script
-        id="Script1"
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-      />
-      <Script
-        id="Script2"
-        dangerouslySetInnerHTML={{
-          __html: `
-               (adsbygoogle = window.adsbygoogle || []).push({
-                   google_ad_client: "ca-pub-4979943891567316",
-                   enable_page_level_ads: true
-              });
-                `,
-        }}
-      />
       <Navbar />
       <Component {...pageProps} />
       <Footer />
