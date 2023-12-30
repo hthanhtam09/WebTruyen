adff90386259ae1e16ed48304db246f7559d85e9
tamht <tamht@kozo-japan.com>
2023-12-30 15:18:21 +0700

    deloy code to hosting

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
-// const { i18n } = require('./next-i18next.config') 
 
 const nextConfig = {
-    images: {
-        unoptimized: true,
-    },
-}
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
