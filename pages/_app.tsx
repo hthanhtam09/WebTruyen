a238df2c9eeef40748e136c7f50c76c1f2fe3858
tamht <tamht@kozo-japan.com>
2023-12-30 22:38:39 +0700

    change position sw file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,13 @@ import '../styles/globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import ClientOnly from './ClientOnly';
+import Script from 'next/script';
 
 function App({ Component, pageProps: { ...pageProps } }: AppProps) {
 
   return (
     <ClientOnly>
+      <Script src='../sw.js' />
       <Navbar />
       <Component {...pageProps} />
       <Footer />
