1786b6dad7dae108644197ff2cc02ecd37816da8
tamht <tamht@kozo-japan.com>
2024-03-10 22:48:08 +0700

    add file verify gg addsense

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -56,7 +56,8 @@ function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
                     src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4979943891567316"
                     crossOrigin="anonymous"
                   ></script>
-                  <meta name="google-site-verification" content="9DKjgYzDLjd5Zi2VrG_REaHHRNf80q52oyzDojjrWMA" />
+                  <meta name="google-adsense-account" content="ca-pub-4979943891567316"></meta>
+                  
                 </>
               )}
             </Helmet>
